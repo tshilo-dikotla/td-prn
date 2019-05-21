9e03f60a7ecbcd4972eea8c07717c2d8b198e011
Leslie <leslie.kep@gmail.com>
2019-05-21 16:21:00 +0200

    remove reference model mixin

--- td_prn/models/maternal_death_report.py
+++ td_prn/models/maternal_death_report.py
@@ -1,17 +1,15 @@
+from edc_action_item.model_mixins.action_model_mixin import ActionModelMixin
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
 from edc_base.sites import SiteModelMixin
 from edc_identifier.managers import SubjectIdentifierManager
-from edc_reference.model_mixins import ReferenceModelMixin
 from edc_search.model_mixins import SearchSlugModelMixin
 
-from edc_action_item.model_mixins.action_model_mixin import ActionModelMixin
 from ..action_items import MATERNAL_DEATH_REPORT_ACTION
 from .death_report_model_mixin import DeathReportModelMixin
 
 
-class MaternalDeathReport(DeathReportModelMixin, ReferenceModelMixin,
-                          ActionModelMixin, SiteModelMixin,
+class MaternalDeathReport(DeathReportModelMixin, ActionModelMixin, SiteModelMixin,
                           SearchSlugModelMixin, BaseUuidModel):
 
     tracking_identifier_prefix = 'MDR'
