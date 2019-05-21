08a882ca854255e2ee686c2ab88d31acc239a070
tumiemosweu <imosweuk@gmail.com>
2019-05-21 10:49:14 +0200

    fix admin init

--- td_prn/admin/__init__.py
+++ td_prn/admin/__init__.py
@@ -1,4 +1,4 @@
 from .infant_death_report_admin import InfantDeathReportAdmin
 from .infant_off_study_admin import InfantOffStudyAdmin
-from .maternal_death_report_admin import DeathReportAdmin
+from .maternal_death_report_admin import MaternalDeathReportAdmin
 from .maternal_off_study_admin import MaternalOffStudyAdmin
