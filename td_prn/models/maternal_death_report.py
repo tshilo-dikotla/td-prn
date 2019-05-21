269454c2e5d5381945bf7d9cd4eda70121416992
tumiemosweu <imosweuk@gmail.com>
2019-05-21 16:17:10 +0200

    remove unused import

--- td_prn/models/maternal_death_report.py
+++ td_prn/models/maternal_death_report.py
@@ -1,20 +1,12 @@
-from django.db import models
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
-from edc_base.model_validators import datetime_not_future
 from edc_base.sites import SiteModelMixin
-from edc_base.utils import get_utcnow
-from edc_constants.choices import YES_NO
 from edc_identifier.managers import SubjectIdentifierManager
-from edc_protocol.validators import datetime_not_before_study_start
 from edc_reference.model_mixins import ReferenceModelMixin
 from edc_search.model_mixins import SearchSlugModelMixin
 
 from edc_action_item.model_mixins.action_model_mixin import ActionModelMixin
-from edc_base.model_fields import OtherCharField
 from ..action_items import MATERNAL_DEATH_REPORT_ACTION
-from ..choices import MED_RESPONSIBILITY, HOSPITILIZATION_REASONS
-from ..choices import SOURCE_OF_DEATH_INFO, CAUSE_OF_DEATH_CAT
 from .death_report_model_mixin import DeathReportModelMixin
 
 
